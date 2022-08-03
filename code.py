df4029a18d2f1feb641e5cfe8614a72ca4c46fea
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-08-03 17:57:52 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+
+
+
+def convertTupleToString(tuple):
+    tuple = list(tuple)  #the tuple need to be a list
+    stringTotal = ''  #string to recive all datas 
+
+    for i in tuple: 
+        string = str(i) #making the data a string 
+        stringTotal = stringTotal + ',' + string #concated the string in the stringTotal
+
+    if stringTotal[0] == ',': #remove the first ',' 
+        stringTotal = stringTotal[1:] 
+
+    return stringTotal
