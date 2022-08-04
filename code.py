27cb004267c47067ba5c66c646b9119df28217a3
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-08-04 14:47:35 -0300

    Update code.py

--- code.py
+++ code.py
@@ -20,21 +20,21 @@ def convertTupleToString(tuple):
     
 
 def CSVfile(data):
-    fileExiste = exists("/home/user/yourpath") #verify if the file exist, return a bolean 
+    fileExiste = exists("/home/user/yourpath/CSVFILE.csv") #verify if the file exist, return a bolean 
     
     dataString = convertTupleToString(data) #transform the tuple in a string with the ','
     
     header = "id,time,value,otherValue,someValue"  #header for the  csv 
 
     if fileExiste == False: #if the file dont existe, need create and write the HEADER 
-        f = open("/home/user/yourpath", "w")   #path where the file is gonna be save 
+        f = open("/home/user/yourpath/CSVFILE.csv", "w")   #path where the file is gonna be save 
         f.write(header)    
         f.write('\n')
         f.write(dataString)
         f.write('\n')
         f.close()
     else: #if the file existe, just need to write the dataString
-        f = open("/home/user/yourpath", "a")
+        f = open("/home/user/yourpath/CSVFILE.csv", "a")
         f.write(dataString)
         f.write('\n')
         f.close()
