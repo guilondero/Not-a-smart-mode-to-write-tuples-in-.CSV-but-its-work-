61bb71f3750037baccab85dfdc8c685a31a80988
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-08-04 10:40:22 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 import csv
 import os
+from os.path import exists 
+
+
 
 
 def convertTupleToString(tuple):
@@ -13,4 +16,24 @@ def convertTupleToString(tuple):
     if stringTotal[0] == ',': #remove the first ',' 
         stringTotal = stringTotal[1:] 
 
-    return stringTotal
+    return stringTotal]
+    
+
+def CSVfile(data):
+    fileExiste = exists("/home/user/yourpath") #verifica se o arquivo existe
+    
+    dataString = convertTupleToString(data) #transform the tuple in a string with the ','
+
+    if fileExiste == False: #se o arquivo nao existe é necessario criar o cabeçalho e inserir os dados
+        f = open("/home/user/yourpath", "w")
+        f.write("id,data,datasys,longitude,latitude,odometro,velocidade,ignicao,rpm,transparent_data,ibutton,dir,in1,out1,out2,gps,gprs,horimetro,voltagem,temperatura,contador,motivo,pacote")
+        f.write('\n')
+        f.write(dataString)
+        f.write('\n')
+        f.close()
+    else: #se o arquivo existe é necessario inserir os dados
+        f = open("/home/londero/gateway/python/GV53MG/csvTrack.csv", "a")
+        f.write(dataString)
+        f.write('\n')
+        f.close()
+
